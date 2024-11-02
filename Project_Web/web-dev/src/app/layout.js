bfc34482db4323bb5e69819620faf9d9d794a33a
wijit1 <kawkawza20@gmail.com>
2024-11-02 21:14:23 +0700

    add new page and add footer
    User
    Product
    Login

--- Project_Web/web-dev/src/app/layout.js
+++ Project_Web/web-dev/src/app/layout.js
@@ -1,4 +1,5 @@
 
+import Footer from "@/components/Footer";
 import Navbar from "../components/Navbar";
 import "./globals.css";
 export default function RootLayout({ children }) {
@@ -7,7 +8,7 @@ export default function RootLayout({ children }) {
       <body>
        <Navbar/> 
        {children}
-       
+       <Footer/>
       </body>
     </html>
   );
