1d3cf7c11f409f7dc37c66dbe49bc41447389a72
Sorrakit_Bank <106497187+Sorrakit-Bank100@users.noreply.github.com>
2024-11-02 19:00:23 +0700

    add home page  Next js

---
+++ Project_Web/web-dev/src/app/detail/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function detail() {
+ return (
+   <div className="text-3xl">
+     This is Detail Page 
+   </div>
+  );
+}
---
+++ Project_Web/web-dev/src/app/search/page.jsx
@@ -0,0 +1,11 @@
+
+
+export default function search() {
+ return (
+   <div className="text-3xl">
+    This is Search Page !!!
+   
+   </div>
+
+  );
+}
