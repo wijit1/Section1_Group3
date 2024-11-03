e62aeedc06f9dd5fc9f57e393a37918be49d99a9
Sorrakit_Bank <106497187+Sorrakit-Bank100@users.noreply.github.com>
2024-11-03 07:07:56 +0700

    Add ShopContext

--- Project_Web/web-dev/src/app/layout.js
+++ Project_Web/web-dev/src/app/layout.js
@@ -1,13 +1,16 @@
 
 import Navbar from "../components/Navbar";
 import "./globals.css";
+import { ShopContextProvider } from "@/context/ShopContext";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-       <Navbar/> 
-       {children}
-       
+        <ShopContextProvider>
+          <Navbar />
+          {children}
+        </ShopContextProvider>
+
       </body>
     </html>
   );
