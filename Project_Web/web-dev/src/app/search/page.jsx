13da0e5f59fd33885d1f321390a3aa9728dfe861
Sorrakit_Bank <106497187+Sorrakit-Bank100@users.noreply.github.com>
2024-11-03 07:27:59 +0700

    Create  Search

--- Project_Web/web-dev/src/app/search/page.jsx
+++ Project_Web/web-dev/src/app/search/page.jsx
@@ -1,10 +1,16 @@
 
 
+import { SearchBar } from "@/components/SearchBar"
+import { Category } from "@/components/Category"
+import { Collection } from "@/components/Collection"
 export default function search() {
- return (
-   <div className="text-3xl">
-    Search 
-   </div>
-
-  );
+  return (
+    <div className="flex flex-col mt-5 gap-10">
+      <SearchBar/>
+      <div className="flex gap-4 pt-10 px-16">
+        <Category/>
+        <Collection/>
+      </div>
+    </div>
+  )
 }
