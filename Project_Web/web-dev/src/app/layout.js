1d66cd5520efbe780d41b033aaa137f9ffaaebe0
Sorrakit_Bank <106497187+Sorrakit-Bank100@users.noreply.github.com>
2024-11-03 07:18:09 +0700

    Add ShopContext

--- Project_Web/web-dev/src/app/layout.js
+++ Project_Web/web-dev/src/app/layout.js
@@ -2,14 +2,17 @@
 import Footer from "@/components/Footer";
 import Navbar from "../components/Navbar";
 import "./globals.css";
+import { ShopContextProvider } from "@/context/ShopContext";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
 
-       <Navbar/> 
-       {children}
-       <Footer/>
+      <ShopContextProvider>
+          <Navbar />
+          {children}
+          <Footer/>
+      </ShopContextProvider>
 
       </body>
     </html>
---
+++ Project_Web/web-dev/src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React, { createContext } from 'react'
+import { useState  } from 'react';
+
+import { products } from '../../assets/assets';
+
+export const ShopContext = createContext();
+
+export const ShopContextProvider = (props)=>{
+    const [search,setSearch] = useState('');
+
+
+    const value = {
+        search,setSearch,products
+    };
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
