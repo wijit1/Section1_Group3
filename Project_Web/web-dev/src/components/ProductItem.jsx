39d60a8c04bda4cebbcd1a080e0d2b6a33e9840a
wijit1 <kawkawza20@gmail.com>
2024-11-07 18:33:19 +0700

    fix icon coin position and better

--- Project_Web/web-dev/src/components/ProductItem.jsx
+++ Project_Web/web-dev/src/components/ProductItem.jsx
@@ -2,22 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 import { assests } from '../../assets/assets'
 
-export const ProductItem = ({product}) => {
+export const ProductItem = ({ product }) => {
     return (
-
         <div className="flex flex-col gap-2 px-7 py-8 border-4 border-gray-800 rounded-xl bg-gray-100">
             <div className="w-[248px] h-[248px] bg-red-800">
-                <Image className='w-full h-full'  src={product.Picture[0]} />
+                <Image className='w-full h-full' src={product.Picture[0]} alt={product.Name} />
             </div>
-            <div className="text-2xl font-bold ">
-                <p className="font-sans">{product.Name} </p>
+            <div className="text-2xl font-bold">
+                <p className="font-sans">{product.Name}</p>
             </div>
-            <div className="flex justify-between items-center ">
-                {/* Icon coin  */}
-                <Image src={assests.coin} width={40} height={40} />
-                <p className="text-3xl font-mono">{product.Price}$</p>
+            <div className="flex  items-center">
+                                {/* Icon Coin */}
+                <Image src={assests.coin} width={40} height={40} alt="coin" class ="w-7 h-7" />
+                <p className="text-3xl font-mono ml-2">{product.Price}</p>
             </div>
         </div>
-
     )
 }
