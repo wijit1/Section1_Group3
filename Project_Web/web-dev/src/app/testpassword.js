2f3d3bb9dad08e314cfa76001c119de26cef60cc
wijit1 <kawkawza20@gmail.com>
2024-11-15 19:45:15 +0700

    del test

--- Project_Web/web-dev/src/app/testpassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = '123';
-const hashedPassword = '$2a$10$E6NfXc0KjZO4lkEqjWIRcuQdl/F9Rm6vEXlkN5mbYo91mWr6EvYPK';
-
-bcrypt.compare(password, hashedPassword, (err, isMatch) => {
-    if (err) throw err;
-    console.log(isMatch ? 'รหัสผ่านตรงกัน' : 'รหัสผ่านไม่ตรงกัน');
-});
