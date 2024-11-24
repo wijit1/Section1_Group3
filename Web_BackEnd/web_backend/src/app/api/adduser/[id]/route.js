54cd5adb66973a0c6af1629aa6ea531ae61f4283
Sorrakit_Bank <106497187+Sorrakit-Bank100@users.noreply.github.com>
2024-11-24 10:25:23 +0700

    Edit Add user Account page

--- Web_BackEnd/web_backend/src/app/api/adduser/[id]/route.js
+++ Web_BackEnd/web_backend/src/app/api/adduser/[id]/route.js
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import { createConnection } from "@/lib/db";
 
-export async function PUT(req,{params}){
-    const {id}= params;
-    const formData = await req.formData(); 
+export async function PUT(req, { params }) {
+    const { id } = params;
+    const formData = await req.formData();
 
-    try{
+    try {
         const name = formData.get('name');
         const email = formData.get('email');
         const address = formData.get('address');
@@ -14,20 +14,29 @@ export async function PUT(req,{params}){
         const image = formData.get('imageProfile')
         const password = formData.get('user_password')
 
-        
 
-        const imageByteData = await image.arrayBuffer();
-        const buffer = Buffer.from(imageByteData);
-        const db = await createConnection();
-        const [result] = await db.query(
-            'UPDATE user_acc SET User_Name = ?, Addrss = ? , Email = ? ,Tel = ? , Brith_Date = ?,profile_image = ? , User_password = ? WHERE User_ID = ?  '
-            , [name, address,email,tel,birth_date,buffer,password,id]);
-    
-        return NextResponse.json({result,message:"Update User success",image})
+        if (image instanceof File && image.size > 0) {
+            const imageByteData = await image.arrayBuffer();
+            const buffer = Buffer.from(imageByteData);
+            const db = await createConnection();
+            const [result] = await db.query(
+                'UPDATE user_acc SET User_Name = ?, Addrss = ? , Email = ? ,Tel = ? , Brith_Date = ?,profile_image = ? , User_password = ? WHERE User_ID = ?  '
+                , [name, address, email, tel, birth_date, buffer, password, id]);
+
+            return NextResponse.json({ result, message: "Update User success" })
+        } else {
+            const db = await createConnection();
+            const [result] = await db.query(
+                'UPDATE user_acc SET User_Name = ?, Addrss = ? , Email = ? ,Tel = ? , Brith_Date = ?, User_password = ? WHERE User_ID = ?  '
+                , [name, address, email, tel, birth_date, password, id]);
+
+            return NextResponse.json({ result, message: "Update User success" })
+        };
 
 
-    }catch(error){
+
+    } catch (error) {
         console.log("Error ");
-        return NextResponse.json({error:error.message})
+        return NextResponse.json({ error: error.message })
     }
 }
